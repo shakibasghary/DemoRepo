ad82b21a030f16e04ccf9fc6f0a3fbe6cb549128
shakibasghary <shakib.007@gmail.com>
2020-10-26 16:21:28 -0700

    Second Commit

---
+++ src/main/java/com/demoproject1/com/demoproject1/App.java
@@ -0,0 +1,13 @@
+package com.demoproject1.com.demoproject1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
